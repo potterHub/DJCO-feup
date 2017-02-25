554c4d5e26f689f0039f9cdbb73db2d3961e0a84
Pedro Moura <up201306843@fe.up.pt>
2017-02-25 19:36:41 +0000

    testing stuff

---
+++ ImpossibleFEUP/Assets/Script/ScrollScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScene : MonoBehaviour {
+    private Rigidbody2D ground;
+    
+    // Use this for initialization
+    void Start()
+    {
+        ground = GetComponent<Rigidbody2D>();// get the kinematic rigid body from the ground
+
+        ground.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);// scroll velocity
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
